e32e5a6b1bb9226a1d580a2b7c4b4d41745a2a89
Artem Avakyan <artem@aprint.pro>
2021-03-19 05:15:22 +0300

    предпоказ

--- src/js/product.js
+++ src/js/product.js
@@ -1,36 +1,40 @@
+import Cookies from "js-cookie";
+
 const currentProduct = $(".product.ms2_form input[name='id']").val() || null;
-
+const $selectionButtons = $(".j-countButtons");
 let min = parseFloat($(".j-cnt-min").text());
-const max = parseFloat($(".j-cnt-max").text());
-
+let max = parseFloat($(".j-cnt-max").text());
 let currentTotal = +max;
 let cart = {};
-const btnAddedList = [];
-const $selectionButtons = $(".j-countButtons");
+let btnAddedList = [];
+
+const $container = $(".add-to-cart__status");
 
 $("#product_price").val(min).attr({ min: min, max: max });
 
-function getCart() {
+function getCart(btnList) {
   const productId = $('#msProduct input[name="id"]').val();
   if (productId) {
+    const postData = {
+      id: productId,
+      key: cart.key,
+      btn_list: btnList || [],
+    };
+
     $.post(
       "/gettotal",
       {
-        cart: JSON.stringify({
-          id: productId,
-          btn_list: btnAddedList,
-          key: cart.key,
-        }),
+        cart: JSON.stringify(postData),
       },
       function (data) {
         const currentProduct = $(".ms2_form input[name='id']").val();
-        const $container = $(".add-to-cart__status");
+
         cart = JSON.parse(data);
         cart.count = cart.count || 0;
 
-        cart.id === currentProduct
-          ? $container.addClass("active")
-          : $container.removeClass("active");
+        if ($container) {
+          if (cart.id === currentProduct) $container.addClass("active");
+        }
 
         currentTotal = max - cart.count;
         if (cart.count >= min) {
@@ -56,18 +60,30 @@ function getCart() {
 miniShop2.Callbacks.add("Cart.add.before", "restrict_cart1", function () {
   const current = +$("#product_price").val();
 
-  // if (cart.id === currentProduct) {
-  //   console.log("уже в корзине");
-  // }
-  console.log(`cart - ${cart}`);
-
-  console.log(`current - ${current}`);
-  console.log(`min - ${min}`);
-  console.log(`max - ${max}`);
-  if (current > max) {
-    miniShop2.Message.error("Вы пытаетесь добавить больше максимального веса");
-    return false;
+  if ($container) {
+    if (current > max) {
+      miniShop2.Message.error(
+        "Вы пытаетесь добавить больше максимального веса"
+      );
+      return false;
+    } else {
+      $container.addClass("active");
+    }
   }
+
+  const btnAddedList = [];
+  if ($selectionButtons.length) {
+    const $buttons = $selectionButtons.find("input:checked");
+
+    $buttons.each(function () {
+      btnAddedList.push({
+        id: $(this).attr("id"),
+        value: $(this).val(),
+      });
+    });
+    Cookies.set("btn_list", JSON.stringify(btnAddedList));
+  }
+  // getCart(btnAddedList);
 });
 
 miniShop2.Callbacks.add(
@@ -76,33 +92,21 @@ miniShop2.Callbacks.add(
   function (response) {
     const count = response.data.total_count;
     currentTotal = max - count;
-
-    if ($selectionButtons.length) {
-      const $buttons = $selectionButtons.find("input:checked");
-      $buttons.each(function () {
-        $(this).val;
-        btnAddedList.push({
-          id: $(this).attr("id"),
-          value: $(this).val(),
-        });
-      });
-    }
-    getCart();
+    Cookies.remove("btn_list");
   }
 );
 
 if ($selectionButtons.length) {
-  const buttonList = [...$selectionButtons.find("input")];
+  $("#product_price").attr("readonly", "readonly");
+  let buttonList = [...$selectionButtons.find("input")];
   buttonList.sort((a, b) => +a.value - +b.value);
+
   $(buttonList).first().attr("checked", "checked");
   $(document).on("change", ".j-countButtons input", function () {
     const count = +$(this).val();
     const currentValue = +$("#product_price").val();
 
     if ($(this).is(":checked")) {
-      console.log(`count - ${count}`);
-      console.log(`currentValue - ${currentValue}`);
-      console.log(`разница - ${currentValue - count}`);
       $("#product_price").val((currentValue + count).toFixed(2));
     } else {
       $("#product_price").val((currentValue - count).toFixed(2));
